471c85b31a1081fea0cb55231c242ae567236d02
Your Name <you@example.com>
2019-04-05 23:43:10 -0400

    Entity Framework instalado2

---
+++ Productos y clientes/Productos y clientes/Models/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Productos_y_clientes.Models
+{
+    public class DataContext:DBContext
+    {
+    }
+}
